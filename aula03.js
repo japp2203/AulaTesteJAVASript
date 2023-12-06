bf251ccbc74fba4f5bc343dcebfb6bf1a072af19
JESÚS ALBERTO PÉREZ PEÑA <japp2203@gmail.com>
2023-12-05 21:24:44 -0300

    AulaTestJAVAScript
    criação de primeira aula de JS

---
+++ aula03.js
@@ -0,0 +1,18 @@
+// const = constante
+// corchetes se usan para: ?????????
+
+const investimentoMensal = [1000, 2000, 3000, 1000, 5000]
+
+// vamos usar muito: console.log, tem a ver com a impressão na tela
+// usamos apostrofe `` por: ??????????
+
+
+console.log(`O Valor Investido no mês de Janeiro foi:   `)
+
+// amanhã vamos saber a diferença e o uso adecuado
+// ()
+// []
+// {}
+// ""
+// ''
+// ``
