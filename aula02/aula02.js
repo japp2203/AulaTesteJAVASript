8ade27d2ba80b3331c47361cb050b5d9c5ed70bf
JESÚS ALBERT PÉREZ PEÑA <japp2203@gmail.com>
2023-12-06 21:04:34 -0300

    Aula03b
    ate aqui aprendimos a usar os operadores matematicos basicos

---
+++ aula02/aula02.js
@@ -0,0 +1,44 @@
+// em js o simbolo = significa "recebe"
+// para delarar var com textos pode usar '' ou ""
+// console.log imprime na tela | "disparo"✨
+// com node.\[nome de arquivo.js] imprime na tela
+// posso declarar variaveis de três formas:...
+// (var):, 
+// (const): valor fixo, nunca irá se alterar e 
+// (let): troca o valor da variable, declara variavel não fixa, pode ser alterada futuramente
+
+// variaveis numericas não precisam aspas "'
+// se uma variable tipo const foi declarado, não muda quando outra var tenta trocar o valor, ele vai ficar constante
+
+
+//var idade = "eu tenho 18 anos pppppppppppp";
+//console.log(idade);
+
+// const nome = ("igor");
+nome = ("JESUS");
+console.log(nome);
+
+//nas boas pratica spara declarra variaveis compostas de masi de uma palavra, a segunda deve iniciar com maiusculas
+
+const sexo = ("femenino");
+console.log(sexo);
+
+const mensagemTela = ('Este texto é um exemplo para podermos praticar o exercicio');
+console.log (mensagemTela); 
+console.log (mensagemTela); 
+console.log (mensagemTela); 
+console.log (mensagemTela); 
+console.log (mensagemTela); 
+
+let mensagemExemplo = ('Este mensagem é um teste')
+console.log (mensagemExemplo); 
+
+// para trocar o valor da variavel tipo (let), não precisa escrever a sintaxe ou comando de novo, pois tambem não funcionará
+
+mensagemExemplo = ('Estou alterando por motivos de contingencia')
+console.log (mensagemExemplo); 
+
+
+
+
+
---
+++ aula02/aula03b.js
@@ -0,0 +1,29 @@
+//operadores javascrip
+
+//(+) = adição
+//(-) = substração
+//(/) = divisão
+//(*) = multiplicação 
+
+const resultadoSoma = 25 + 35;
+console.log(resultadoSoma);
+
+const texto1 = 'ola, meu nome é Igor,';
+const texto2 = ' Eu tenho 24 anos';
+
+const textoFinal = texto1 + texto2; //paramos por aqui, se usarmos sinal de (-) vai dar merda
+console.log(textoFinal);
+
+const resultadoSubstracao = 10 - 1090;
+console.log(resultadoSubstracao);
+
+const resultadoDivisao = 144 / 12;
+console.log(resultadoDivisao);
+
+const resultadoMultiplicaçao = 12 * 12;
+console.log(resultadoMultiplicaçao);
+
+const resultadoOperaçao = (((10 - 1090) / 12) * 125)
+console.log(resultadoOperaçao);
+
+
