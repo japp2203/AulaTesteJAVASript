b73b83ad019815d0ec6c158ccbe0781b5d6b1e55
JESÚS ALBERTO PÉREZ PEÑA <japp2203@gmail.com>
2023-12-07 21:00:47 -0300

    AulaTesteJAVAScript
    hoy criamos a Aula06
    iniciamos com array

---
+++ aula04/aula04.js
@@ -0,0 +1,29 @@
+//operadores javascrip
+
+//(+) = adição
+//(-) = substração
+//(/) = divisão
+//(*) = multiplicação 
+
+const resultadoSoma = 25 + 35;
+console.log(resultadoSoma);
+
+const texto1 = 'ola, meu nome é Igor,';
+const texto2 = ' Eu tenho 24 anos';
+
+const resultadoSubstracao = 10 - 1090;
+console.log(resultadoSubstracao);
+
+const resultadoDivisao = 144 / 12;
+console.log(resultadoDivisao);
+
+const resultadoMultiplicaçao = 12 * 12;
+console.log(resultadoMultiplicaçao);
+
+const resultadoOperaçao = ((((10 - 1090) / 12) * 125)+11252)-2;
+console.log(resultadoOperaçao);
+
+// aula 07/12/2023
+const textoFinal = texto1 + texto2; //paramos por aqui, se usarmos sinal de (-) vai mostrar o erro "NaN" (Não é um Número)
+console.log(textoFinal);
+ 
---
+++ aula04/aula05.js
@@ -0,0 +1,10 @@
+const texto1 = 'Aqui temos uma STRING normal';
+const texto2 = "já esta, apesar das aspas duplas ao inves das simples, também é uma STRING [texto corrido]";
+// numeros não vão encerrados entre aspas nem semples nem duplas
+const numeroRegistrar = 31472;
+const stringTemplate = `O número que quis registrar foi o ${numeroRegistrar}`; //se usamos aspas simples ao inves de APOSTROFE vai dar erro \
+
+console.log(stringTemplate);
+
+
+
